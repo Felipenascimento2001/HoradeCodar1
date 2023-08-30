688dcec35ed0f92e01c3c0f213964ede4e3e84f2
Felipe Nascimento <felq.nascimento2012@hotmail.com>
2023-08-30 17:27:13 -0300

    hora de codar 1

---
+++ script.js
@@ -0,0 +1,60 @@
+//1:Escreva um programa em Javascript e crie uma variável chamada "nome_do_carro" e atribua-a um valor "Fusca". Exiba-a ao usuário.
+
+//var nome_de_carro = "Fusca";
+//console.log(nome_de_carro);
+
+//2:Escreva um programa em Javascript em que o usuário informe o seu nome e exiba a mensagem "Olá, [NomeDoUsuario]".
+
+//var nome = prompt();
+
+//console.log("Olá, " + nome);
+
+//3:Escreva um programa em Javascript em que o usuário informe o seu nome e em seguida o programa
+// perguntará a idade do usuário. Agora o programa deve exibir a mensagem "Olá, [NomeDoUsuario], sua idade é [idade]".
+
+//var nome = prompt("Digite seu nome: ");
+//var idade = prompt("Digite sua idade: ");
+
+//console.log("Seu nome é: " + nome + "; sua idade é: " + idade + " anos");
+
+//4:Considerando a figura abaixo, escreva um programa para cada forma que calcule e exiba em 
+//tela cada uma de suas respectivas áreas. O usuário irá informar os valores de cada variável.
+
+/*let A = 0;
+let baseR = prompt("Informe a Base para o Retângulo:");
+let alturaR = prompt("Informe a Altura o Retângulo:");
+A = baseR * alturaR;
+window.document.writeln(`A área do Retângulo é de: ${A} |`);
+
+let ladoQ = prompt("Informe o Lado do Quadrado:");
+A = ladoQ * ladoQ;
+window.document.writeln(`A área do Quadrado é de: ${A} |`);
+
+let diagonal_maior = prompt("Informe o Diagonal Maior para o Losango:");
+let diagonal_menor = prompt("Informe o Diagonal Menor para o Losango:");
+A = (diagonal_maior * diagonal_menor) / 2;
+window.document.writeln(`A área do Losango é de: ${A} |`);
+
+let baseMaior = prompt("Informe a Base Maior para o Trapézio:");
+let baseMenor = prompt("Informe a Base Menor para o Trapézio:");
+let alturaT = prompt("Altura Trapézio:");
+A = ((baseMaior + baseMenor) * alturaT) / 2;
+window.document.writeln(`A área do Trapézio é de: ${A} |`);
+
+let baseP = prompt("Informe a Base para o Paralelogramo:");
+let alturaP = prompt("Altura Paralelogramo:");
+A = baseP * alturaP;
+window.document.writeln(`A área do Paralelogramo é de: ${A} |`);
+
+let baseTri = prompt("Informe a Base Maior para o Trapézio:");
+let alturaTri = prompt("Altura Triângulo:");
+A = (baseTri * alturaTri) / 2;
+window.document.writeln(`A área do Triângulo é de: ${A} |`);
+
+const pi = 3.14159;
+let raio = prompt("Digite o Raio do Círculo:");
+A = pi * (raio * raio);
+window.document.writeln(`A área do Círculo é de: ${A} |`);*/
+
+
+
